6975ad3a8c56bacf4085e060e95c2af0bd932ef3
Yirule <2356758998@qq.com>
2023-04-20 14:08:13 +0800

    put train function into main

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('yolov8m-pose.yaml')  # build a new model from YAML
+# model = YOLO('yolov8m-pose.yaml')  # build a new model from YAML
 # model = YOLO('yolov8n-pose.pt')  # load a pretrained model (recommended for training)
-# model = YOLO('yolov8n-pose.yaml').load('yolov8n-pose.pt')  # build from YAML and transfer weights
+model = YOLO('yolov8n-pose.yaml').load('runs/pose/train/weights/best.pt')  # build from YAML and transfer weights
 
-# Train the model
-model.train(data='armor-four-points.yaml', epochs=100, imgsz=640)
+if __name__ == '__main__':
+    # Train the model, device = 0 for GPU, device = 'cpu' for CPU
+    model.train(data='armor-four-points.yaml', epochs=100, imgsz=640, device = 0)
