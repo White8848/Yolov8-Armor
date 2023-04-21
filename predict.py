be266dbe7c46a4c0c77d83442a58451a0f86d20c
Yirule <2356758998@qq.com>
2023-04-21 15:27:05 +0800

    change predict settings

--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 # Load a model
 # model = YOLO('yolov8n-pose.pt')  # load an official model
-model = YOLO('runs/pose/train/weights/best.pt')  # load a custom model
+model = YOLO('runs/pose/train6/weights/best.pt')  # load a custom model
 
 # Predict with the model
-results = model('dataset/tests/armor_1_test.mp4', save = True, device = 0,hide_conf = True, hide_labels = True)  # predict on an image
+results = model('dataset/images/train/280.jpg', save = True, device = 0)  # predict on an image
