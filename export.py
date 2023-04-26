2d93e600354e914f772b4efbb44b4ee2e9320e58
Yirule <2356758998@qq.com>
2023-04-26 17:55:23 +0800

    update path

--- export.py
+++ export.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 # Load a model
 # model = YOLO('yolov8n.pt')  # load an official model
-model = YOLO('runs/pose/train6/weights/best.pt')  # load a custom trained
+model = YOLO('runs/pose/train/weights/best.pt')  # load a custom trained
 
 # Export the model
 model.export(format='OpenVINO')
--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 # Load a model
 # model = YOLO('yolov8n-pose.pt')  # load an official model
-model = YOLO('runs/pose/train6/weights/best.pt')  # load a custom model
+model = YOLO('runs/pose/train/weights/best.pt')  # load a custom model
 
 # Predict with the model
-results = model('dataset/images/train/280.jpg', save = True, device = 0)  # predict on an image
+results = model('datasets/armor-four-points/tests/blue_1.jpg', save = True, device = 0)  # predict on an image
